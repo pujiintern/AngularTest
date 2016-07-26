7ee6c0feba60b707588918ef2f79bae9f9f557be
chayapon1219 <p.chayapon@hotmail.com>
2016-07-26 10:42:49 +0700

    Finished Committed

--- js/AngularChat.js
+++ js/AngularChat.js
@@ -1,10 +1,11 @@
 
-var myApp  =  angular.module('myApp', []);
+var myApp  =  angular.module('myApp', ['ngMaterial']);
 
 myApp.controller('jsonCtrl', function($scope, $http){
     $http.get('chatApp.json').success(function (data){
         $scope.chat = data;
     });
+
     $scope.chatNo1 = '';
     $scope.chatNo2 = '';
     
@@ -27,6 +28,8 @@ myApp.controller('jsonCtrl', function($scope, $http){
      });
          httpPost();
      };
+
+
      $scope.addChat2 = function(){      
      var currentdate = new Date(); 
      var datetime = currentdate.getDate() + "/"
@@ -43,53 +46,7 @@ myApp.controller('jsonCtrl', function($scope, $http){
          httpPost();
      };
     
-    
-        
-   
-   
-
-
-
-
-   /* $scope.saveToPc = function (data, filename) {
-        data = {
-    "userId": 100,
-    "id": 100,
-    "title": "test",
-    "body": "test\nanimi doloribus cum rerum quas et magni\net hic ut ut commodi expedita sunt"
-  }
-  if (!data) {
-    console.error('No data');
-    return;
-  }
-
-  if (!filename) {
-    filename = 'TestJSON.json';
-  }
-
-  if (typeof data === 'object') {
-    data = JSON.stringify(data, undefined, 2);
-  }
-
-  var blob = new Blob([data], {type: 'text/json'});
-
-  // FOR IE:
-
-  if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-      window.navigator.msSaveOrOpenBlob(blob, filename);
-  }
-  else{
-      var e = document.createEvent('MouseEvents'),
-          a = document.createElement('a');
-
-      a.download = filename;
-      a.href = window.URL.createObjectURL(blob);
-      a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
-      e.initEvent('click', true, false, window,
-          0, 0, 0, 0, 0, false, false, false, false, 0, null);
-      a.dispatchEvent(e);
-  }
-};*/
+ 
 });
 
 
